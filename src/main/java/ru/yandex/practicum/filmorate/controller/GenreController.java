cbec5df126ef44192e539ec5d01828ed568505ef
Artem Oleshko <freedom2323232323@gmail.com>
2024-12-01 16:30:17 +0300

    исправления после ревью

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -7,32 +7,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
+import ru.yandex.practicum.filmorate.service.GenreService;
 
 import java.util.List;
 
-// Аннотация для логирования
 @Slf4j
-// Аннотация для REST-контроллера, который будет обрабатывать запросы о жанрах
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/genres")
 public class GenreController {
+    private final GenreService genreService; // Инъекция GenreService
 
-    // для работы с жанрами
-    private final GenreDbStorage genreDbStorage;
-
-    // Получение списка всех жанров
     @GetMapping
     public List<Genre> getGenresList() {
         log.info("Поступил запрос на получение списка жанров.");
-        return genreDbStorage.getGenres();
+        return genreService.getGenres(); // Используем сервис
     }
 
-    // Получение жанра по идентификатору
     @GetMapping("/{id}")
     public Genre getGenre(@PathVariable int id) {
         log.info("Поступил запрос на получение жанра по id: {}", id);
-        return genreDbStorage.getGenreById(id);
+        return genreService.getGenre(id); // Используем сервис
     }
 }
